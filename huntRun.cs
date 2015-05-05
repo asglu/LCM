23969e3103b920680ca9d9dbdd7b9e599e1a2f8a
ktavui <ktavui@hawaii.edu>
2015-05-05 01:16:26 -1000

    Loomed features added
    DONE:
    ocean color change
    
    TODO:
    check splatter animation condition after collision
    turn on/off sounds
    sync headGauge with highness metric

--- huntRun.cs
+++ huntRun.cs
@@ -13,25 +13,45 @@ public class huntRun : MonoBehaviour
 	private float leftX = -5.97f;
 	private float increment;
 	public Text highText;
+	public Text pointText;
 	public static float high;
 	private static int points;
-	public Animator anim;
-	public AudioClip otherClip;
+	public Animator hunt;
+	public GameObject spray;
+	public GameObject bloodWater;
+	public GameObject pakalolo;
+	public GameObject clash;
+
 	
 	void start ()
 	{
 		high = 50;
 		float h = -Input.GetAxis ("Horizontal");
 		transform.Translate (h, 0, 0);
-		anim = GetComponent<Animator> ();
-		//AudioSource audio = GetComponent<AudioSource> ();
-	}//start
+		spray.gameObject.SetActive (false);
+		bloodWater.gameObject.SetActive (false);
+		//pakalolo.gameObject.SetActive (true);
+		//clash.gameObject.SetActive (false);
 
+		//hunter
+		hunt = GetComponent<Animator> ();
+
+	}//start
+	
 	void Update ()
 	{
 		high = high - Time.deltaTime * 1;
 		SetHighText ();
-
+		SetPointText ();
+		//set splat animation
+		if (hunt.GetBool("close") == true){
+			spray.gameObject.SetActive (true);
+		}//if
+		//stop animation
+		if (hunt.GetBool("close") == false){
+			spray.gameObject.SetActive (false);
+		}//if
+		
 		//restricts X movement
 		if (transform.position.x > rightX) {
 			transform.position = new Vector3 (rightX, transform.position.y, transform.position.z);
@@ -39,7 +59,7 @@ public class huntRun : MonoBehaviour
 		else if (transform.position.x < leftX) {
 			transform.position = new Vector3 (leftX, transform.position.y, transform.position.z);
 		}//else if
-
+		
 		//movement
 		if (Input.GetKey (KeyCode.LeftArrow)) {
 			increment += (leftSpeed / 100) * Time.deltaTime;
@@ -49,19 +69,20 @@ public class huntRun : MonoBehaviour
 		if (Input.GetKey (KeyCode.RightArrow)) {
 			increment += (rightSpeed / 100) * Time.deltaTime;
 			transform.position = Vector3.Lerp (transform.position, transform.position + Vector3.right, increment);
-
+			
 		}//if
 	}//update
-
+	
 	void FixedUpdate ()
 	{
-		
 		if (high > 50) {
 			//godmode
-			anim.SetBool ("fly", true);
+			hunt.SetBool ("fly", true);
+			pakalolo.gameObject.SetActive (false);
+			clash.gameObject.SetActive (true);
+			bloodWater.gameObject.SetActive (true);
 			//change song
 			//audio.Stop();
-			//audio.Play();
 			//change skybox
 			
 			//change water color	
@@ -69,51 +90,71 @@ public class huntRun : MonoBehaviour
 		
 		if (high < 50) {
 			//regularmode
-			anim.SetBool ("fly", false);
+			hunt.SetBool ("fly", false);
+			bloodWater.gameObject.SetActive (false);
 		}//if
 	}//FU
-
+	
 	void OnTriggerEnter (Collider other)
 	{
 		//pickups
-		if (other.gameObject.CompareTag ("drugs") && anim.GetBool ("fly") == false) {
+		if (other.gameObject.CompareTag ("drugs") && hunt.GetBool ("fly") == false) {
 			other.gameObject.SetActive (false);
 			high = high + 5;
 			SetHighText ();
 		}//if
 		//pickups
-		if (other.gameObject.CompareTag ("club") && anim.GetBool ("fly") == false) {
+		if (other.gameObject.CompareTag ("club") && hunt.GetBool ("fly") == false) {
 			other.gameObject.SetActive (false);
 			high = 100;
 			SetHighText ();
 		}//if
 		//NPC
-		if (other.gameObject.CompareTag ("runner") && anim.GetBool ("fly") == false) {
+		if (other.gameObject.CompareTag ("runner") && hunt.GetBool ("fly") == false) {
 			high = high - 5;
 			SetHighText ();
 		}//if
 		//NPC
-		if (other.gameObject.CompareTag ("sam") && anim.GetBool ("fly") == false) {
+		if (other.gameObject.CompareTag ("sam") && hunt.GetBool ("fly") == false) {
 			high = high - 100;
 			SetHighText ();
 		}//if
 		//godmode NPC
-		if (other.gameObject.CompareTag ("runner") && anim.GetBool ("fly") == true) {
-			anim.SetBool ("close", true);
+		if (other.gameObject.CompareTag ("runner") && hunt.GetBool ("fly") == true 
+			|| other.gameObject.CompareTag ("runner") && hunt.GetBool ("close") == true) {
+			//trigger splat animation
+			spray.gameObject.SetActive (true);
+			//trigger hit animation
+			hunt.SetBool ("close", true);
 			other.gameObject.SetActive (false);
 			points = points + 5;
+			SetPointText ();
 		}//if
 		//godmode NPC
-		if (other.gameObject.CompareTag ("sam") && anim.GetBool ("fly") == true) {
-			anim.SetBool ("close", true);
+		if (other.gameObject.CompareTag ("sam") && hunt.GetBool ("fly") == true 
+			|| other.gameObject.CompareTag ("runner") && hunt.GetBool ("close") == true) {
+			//trigger splat animation
+			spray.gameObject.SetActive (true);
+			//trigger hit animation
+			hunt.SetBool ("close", true);
 			other.gameObject.SetActive (false);
 			points = points + 10;
+			SetPointText ();
 		}//if
+		else {
+			hunt.SetBool ("close", false);
+			spray.gameObject.SetActive (false);
+		}
 	}//OnTriggerEnter
-
+	
 	void SetHighText ()
 	{
 		highText.text = "Highness: " + high.ToString ();
 	}//SetHighText
 
+	void SetPointText ()
+	{
+		pointText.text = "Killpoints: " + points.ToString ();
+	}//SetPointText
+	
 }//class
--- lonomode.cs
+++ lonomode.cs
@@ -3,83 +3,31 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class lonomode : MonoBehaviour
-{
-	public KeyCode left = KeyCode.LeftArrow;
-	public KeyCode right = KeyCode.RightArrow;
-	public float leftSpeed = 500;
-	public float rightSpeed = 500;
-	private float rightX = 6.66f;
-	private float leftX = -5.97f;
-	private float increment;
-	public Text highText;
+{	
+	public Animator anim;
 	public static float high;
-	private int points;
-	
+
 	void start ()
 	{
-		high = 100;
-		float h = -Input.GetAxis ("Horizontal");
-		transform.Translate (h, 0, 0);
-		
+		anim = GetComponent<Animator> ();
 	}//start
 
-	void FixedUpdate(){
-
-		while (high < 50) {
-			// change scenes
-			Application.LoadLevel ("Marathon3");
-		}//if
-	}
-	
 	void Update ()
 	{
-		high = high - Time.deltaTime * 1;
-		SetHighText ();
-		
-		//restricts X movement
-		if (transform.position.x > rightX) {
-			transform.position = new Vector3 (rightX, transform.position.y, transform.position.z);
-		}//if
-		else if (transform.position.x < leftX) {
-			transform.position = new Vector3 (leftX, transform.position.y, transform.position.z);
-		}//else if
-		
-		//movement
-		if (Input.GetKey (KeyCode.LeftArrow)) {
-			increment += (leftSpeed / 100) * Time.deltaTime;
-			transform.position = Vector3.Lerp (transform.position, transform.position + Vector3.left, increment);
-		}//if
-		
-		if (Input.GetKey (KeyCode.RightArrow)) {
-			increment += (rightSpeed / 100) * Time.deltaTime;
-			transform.position = Vector3.Lerp (transform.position, transform.position + Vector3.right, increment);
-			
+		if (high <= 10){
+			anim.SetBool("sober", true);
+		}//
+		if (high >= 10){
+			anim.SetBool("buzzed", true);
+		}//
+		if (high >= 20){
+			anim.SetBool("high", true);
+		}//
+		if (high >= 30){
+			anim.SetBool("faded", true);
+		}//
+		if (high >= 40){
+			anim.SetBool("lono", true);
+		}//
 		}//if
 	}//update
-	
-	void OnTriggerEnter (Collider other)
-	{
-		if (other.gameObject.CompareTag ("drugs")) {
-			other.gameObject.SetActive (false);
-			high = high + 5;
-			SetHighText ();
-		}//if
-		if (other.gameObject.CompareTag ("club")) {
-			other.gameObject.SetActive (false);
-			high = 100;
-			SetHighText ();
-		}//if
-
-		if (other.gameObject.CompareTag ("Player")) {
-			other.gameObject.SetActive (false);
-			points = points + 1;
-		}//if
-
-	}//OnTriggerEnter
-	
-	void SetHighText ()
-	{
-		highText.text = "Highness: " + high.ToString ();
-	}//SetHighText
-	
-}//class
